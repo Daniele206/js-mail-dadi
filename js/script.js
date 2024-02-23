58b9a4d6708737bf6c187b829cc01180e53146c2
Daniele206 <danieledeieso2004@gmail.com>
2024-02-23 16:48:07 +0100

    [add] mail to page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-const lsitMail = ['mariorossi@gmail.com', 'rossimario@gmail.com', 'valerossi46@gmail.com'];
+
+
+const lsitMail = ['mariorossi@gmail.com', 'c', 'valerossi46@gmail.com'];
+
+//button
+const playGameButton = document.querySelector('.play-btn')
 
 const userMail = prompt('Inserisci la tua mail:');
 
@@ -11,3 +16,35 @@ if((userMail === lsitMail[0]) || (userMail === lsitMail[1]) || (userMail === lsi
 }
 
 console.log(mailValid);
+
+const title = document.getElementById('title')
+
+if(mailValid === 'yes'){
+  console.log('Sei loggato');
+  title.innerHTML = 'Sei loggato'
+  playGameButton.classList.remove('d-none')
+}else if(userMail === ''){
+  title.innerHTML = `
+  <div class="text-danger my-h-100 d-flex align-items-center justify-content-center">
+    <div class="my-error">
+      Mail mancante
+    </div>
+  </div>
+  `
+  playGameButton.classList.add('d-none')
+}else{
+  console.log('Mail errata, non puoi loggarti');
+  title.innerHTML =`
+  <div class="text-danger my-h-100 d-flex align-items-center justify-content-center">
+    <div class="my-error">
+      Mail errata, non puoi loggarti
+    </div>
+  </div>
+  `
+  playGameButton.classList.add('d-none')
+}
+
+playGameButton.addEventListener('click',function () {
+  console.log('ciao');
+  di
+})
